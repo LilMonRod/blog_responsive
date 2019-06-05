76700bad1f27e869662b5648258fb7132f337b22
Lilliam Montoya <lilmonrod@gmail.com>
2019-06-05 11:58:22 -0600

    arreglo bug de open menu

---
+++ practica2/js/script.js
@@ -0,0 +1,41 @@
+(function() {
+    const btnMenu = document.getElementById('btnMenu');
+    const container = document.getElementById('contHeader');
+    let contMenu = document.querySelectorAll('a.contMenu-js');
+    contMenu = Array.from(contMenu);
+    let value = true;
+    
+    function changeSrc() {
+        btnMenu.removeAttribute('src');
+        if (value) {
+            btnMenu.setAttribute('src', 'img/icons/close-menu.svg');
+        } else {
+            btnMenu.setAttribute('src', 'img/icons/menu.svg');
+        }
+        value = !value;
+    }
+    function closeMenu() {
+        contMenu.forEach(element => {
+            element.style.display = 'none';
+        });
+        container.style.height = 'auto';
+    }
+
+    function openMenu() {
+        contMenu.forEach(element => {
+            element.style.display = 'block';
+        });
+        container.style.height = '-webkit-fill-available';
+    }
+
+    function menu() {
+        if (value) {
+            openMenu();
+        } else {
+            closeMenu();
+        }
+        changeSrc();
+    }
+    btnMenu.addEventListener('touchend', menu);
+    
+}) ();
